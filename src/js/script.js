f87341c882519b9ef88b5730e248bc0824a8fe68
guilherme de araujo <guiam2909@gmail.com>
2025-05-12 14:41:19 -0300

    comecando o arquivo js

---
+++ src/js/script.js
@@ -0,0 +1,40 @@
+function validarEntrada() {
+    let entrada = prompt("Diga um vinho: ");
+    
+}
+
+function vinho(nome, tipo, safra, quantidade) {
+    this.nome = nome;
+    this.tipo = tipo;
+    this.safra = safra;
+    this.quantidade = quantidade;
+}
+
+function estoqueBaixo(quantidade) {
+    return quantidade < 5;
+}
+
+function classificarVinho(ano) {
+    const anoAtual = 2025 
+    const idade = anoAtual - ano;
+
+    if (idade < 5) return "jovem";
+    if (idade < 10) return "amadurecido";
+    if (idade < 15) return "antigo";
+}
+
+function mostrarDados(vinho) {
+    console.log("Dados do Vinho: ");
+    
+}
+
+    console.log("Nome: " + vinho.nome);
+    console.log("Tipo: " + vinho.tipo);
+    console.log("Quantidade: " + vinho.quantidade);
+    console.log("Classificação: " + classificarVinho(vinho.safra));
+    
+    if (estoqueBaixo(vinho.quantidade)) {
+        console.log("Estoque baixo! Reabastecer.");
+    } else {
+        console.log("Estoque suficiente.");
+    }
